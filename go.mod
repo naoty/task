c1119ab58e1f1f206c74a478ccbbed5548603809
Naoto Kaneko <naoty.k@gmail.com>
2019-11-09 20:03:33 +0900

    Reset default styles defined by tview
    By default, each components doesn't use colors configured by
    terminal application.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gdamore/tcell v1.3.0
 	github.com/mattn/go-runewidth v0.0.6 // indirect
 	github.com/rivo/tview v0.0.0-20191018125527-685bf6da76c2
 	github.com/spf13/pflag v1.0.5
--- tui/application.go
+++ tui/application.go
@@ -1,6 +1,7 @@
 package tui
 
 import (
+	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
 
@@ -11,6 +12,8 @@ type Application struct {
 
 // NewApplication initializes and returns a new Application.
 func NewApplication() *Application {
+	resetStyles()
+
 	internal := tview.NewApplication()
 
 	// NOTE: Here is dummy code to demonstrate the application.
@@ -23,4 +26,22 @@ func NewApplication() *Application {
 // Start starts a TUI application.
 func (app *Application) Start() error {
 	return app.Run()
+}
+
+// resetStyles reset default styles for components.
+// Each components doesn't use colors configured by terminal by default.
+func resetStyles() {
+	tview.Styles = tview.Theme{
+		PrimitiveBackgroundColor:    tcell.ColorDefault,
+		ContrastBackgroundColor:     tcell.ColorDefault,
+		MoreContrastBackgroundColor: tcell.ColorDefault,
+		BorderColor:                 tcell.ColorDefault,
+		TitleColor:                  tcell.ColorDefault,
+		GraphicsColor:               tcell.ColorDefault,
+		PrimaryTextColor:            tcell.ColorDefault,
+		SecondaryTextColor:          tcell.ColorDefault,
+		TertiaryTextColor:           tcell.ColorDefault,
+		InverseTextColor:            tcell.ColorDefault,
+		ContrastSecondaryTextColor:  tcell.ColorDefault,
+	}
 }
