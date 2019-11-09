2b327e5a3aaad0c58fcca70b8d7d6073e65ddd43
Naoto Kaneko <naoty.k@gmail.com>
2019-11-09 19:16:36 +0900

    Print help message when -h or --help flag is passed
    The help message follows docopt format.

--- cmd/default.go
+++ cmd/default.go
@@ -3,9 +3,21 @@ package cmd
 import (
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/spf13/pflag"
 )
+
+var helpMessage = `
+Usage:
+  task
+  task -h | --help
+  task -v | --version
+
+Options
+  -h --help     Show this message.
+  -v --version  Show version.
+`
 
 // Default is the default command of this application.
 type Default struct {
@@ -18,12 +30,18 @@ type Default struct {
 // Run starts a default command with arguments.
 func (d *Default) Run(args []string) int {
 	flagset := pflag.NewFlagSet("", pflag.ContinueOnError)
+	helpFlag := flagset.BoolP("help", "h", false, "")
 	versionFlag := flagset.BoolP("version", "v", false, "")
 
 	err := flagset.Parse(args)
 	if err != nil {
 		fmt.Fprintln(d.ErrorWriter, err.Error())
 		return 1
+	}
+
+	if *helpFlag {
+		fmt.Fprintln(d.Writer, strings.Trim(helpMessage, "\n"))
+		return 0
 	}
 
 	if *versionFlag {
