901623b8938da27ec556392471a46a32b71ca7c5
Naoto Kaneko <naoty.k@gmail.com>
2019-12-24 22:53:21 +0900

    Draw a border at top of note

--- tui/note.go
+++ tui/note.go
@@ -1,6 +1,9 @@
 package tui
 
-import "github.com/rivo/tview"
+import (
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
+)
 
 // Note is the text view for note of task.
 type Note struct {
@@ -10,6 +13,21 @@ type Note struct {
 // NewNote initializes and returns a new Note.
 func NewNote() *Note {
 	textView := tview.NewTextView()
+
+	// Draw a border at top of text view
+	textView.SetDrawFunc(func(screen tcell.Screen, x int, y int, width int, height int) (int, int, int, int) {
+		for cx := x; cx < x+width; cx++ {
+			screen.SetContent(
+				cx,
+				y,
+				tview.BoxDrawingsLightHorizontal,
+				[]rune{tview.BoxDrawingsLightHorizontal},
+				tcell.StyleDefault.Foreground(tcell.ColorWhite),
+			)
+		}
+
+		return x, y + 1, width, height - 1
+	})
 
 	return &Note{textView}
 }
